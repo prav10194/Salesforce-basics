69ee3a86b2c74654edbe1d3aabaca0528679e75d
Pranav Bhatia <prav10194@gmail.com>
2023-07-03 20:58:24 -0500

    Update login.py

--- Python/login.py
+++ Python/login.py
@@ -4,10 +4,16 @@ import sys
 
 sf_username = ""
 sf_password = ""
-sf_security_token = "" # Generate this by going into your "personal user" settings > Reset my security token
-domain = "Test" # for sandbox
+sf_security_token = "" # Generate this by going into your "personal user" settings > Reset my security token. The token will be sent to you on email. 
+domain = "Test" # for sandbox. Remove the domain attribute from API otherwise.
+
 sf = Salesforce(username=sf_username, password=sf_password, security_token=sf_security_token, domain=domain)
 
-data = sf.query("Select Id From Account Limit 10")
+data = sf.query("Select Id, Name From Account Limit 10")
 
-print(data)
+# print(data['totalSize'])
+
+# print Id, Name for the records
+for row in data['records']:
+    print(f"Entity name: {row['attributes']['type']}, url: {row['attributes']['url']}")
+    print(f"Id: {row['Id']} and Name: {row['Name']} \n")
